a5d21eb9fda5f4fc6962d4a2600c0d3e99124771
Jasonnor <wujason810@gmail.com>
2016-10-16 02:13:19 +0800

    Add draw point function & coordinate convertion

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.geom.Line2D;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Perceptron {
     private JPanel layoutPanel;
@@ -22,38 +24,58 @@ public class Perceptron {
     private JLabel weightsValue;
     private ArrayList<Float[]> input = new ArrayList<>();
     private ArrayList<Float> output = new ArrayList<>();
+    // TODO - Make it resizeable
+    private int Magnification = 50;
+    private Color[] colorArray = {Color.CYAN, Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.PINK};
 
-    public Perceptron() {
+    private Perceptron() {
         loadButton.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
             FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files(*.txt)", "txt", "text");
             fileChooser.setFileFilter(filter);
             if (fileChooser.showOpenDialog(layoutPanel) == JFileChooser.APPROVE_OPTION) {
-                File loadedFile = fileChooser.getSelectedFile();
-                loadValue.setText(loadedFile.getPath());
-                input.clear();
-                output.clear();
-                try(BufferedReader br = new BufferedReader(new FileReader(loadedFile))) {
-                    String line = br.readLine();
-                    while (line != null) {
-                        String[] lineSplit = line.split("\\s+");
-                        Float[] numbers = new Float[lineSplit.length - 1];
-                        for (int i = 0; i < lineSplit.length - 1; i++) {
-                            numbers[i] = Float.parseFloat(lineSplit[i]);
-                        }
-                        input.add(numbers);
-                        output.add(Float.parseFloat(lineSplit[lineSplit.length - 1]));
-                        line = br.readLine();
-                    }
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
+                loadFile(fileChooser);
             }
         });
     }
 
+    private void loadFile(JFileChooser fileChooser) {
+        File loadedFile = fileChooser.getSelectedFile();
+        loadValue.setText(loadedFile.getPath());
+        input.clear();
+        output.clear();
+        try (BufferedReader br = new BufferedReader(new FileReader(loadedFile))) {
+            String line = br.readLine();
+            while (line != null) {
+                // Split by space or tab
+                String[] lineSplit = line.split("\\s+");
+                // Remove empty elements
+                lineSplit = Arrays.stream(lineSplit).
+                        filter(s -> (s != null && s.length() > 0)).
+                        toArray(String[]::new);
+                Float[] numbers = new Float[lineSplit.length - 1];
+                for (int i = 0; i < lineSplit.length - 1; i++) {
+                    numbers[i] = Float.parseFloat(lineSplit[i]);
+                }
+                input.add(numbers);
+                output.add(Float.parseFloat(lineSplit[lineSplit.length - 1]));
+                line = br.readLine();
+            }
+            coordinatePanel.repaint();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
     private void createUIComponents() {
         coordinatePanel = new GPanel();
+    }
+
+    private Float[] convertCoordinate(Float[] oldPoint) {
+        Float[] newPoint = new Float[2];
+        newPoint[0] = (oldPoint[0] * Magnification) + 250;
+        newPoint[1] = 250 - (oldPoint[1] * Magnification);
+        return newPoint;
     }
 
     public static void main(String[] args) {
@@ -70,13 +92,22 @@ public class Perceptron {
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
     }
-}
 
-class GPanel extends JPanel {
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawLine(250,0,250,500);
-        g.drawLine(0,250,500,250);
+    private class GPanel extends JPanel {
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawLine(250, 0, 250, 500);
+            g.drawLine(0, 250, 500, 250);
+            if (input.size() > 0 && input.get(0).length == 2 && input.size() == output.size()) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setStroke(new BasicStroke(2));
+                for (int i = 0; i < input.size() && i < output.size(); i++) {
+                    Float[] point = convertCoordinate(input.get(i));
+                    g2.setColor(colorArray[Math.round(output.get(i))]);
+                    g2.draw(new Line2D.Double(point[0], point[1], point[0], point[1]));
+                }
+            }
+        }
     }
 }
